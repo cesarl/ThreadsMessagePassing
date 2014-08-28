5259a835a23edd94af5d2aeea4e98e1630c96fca
Cesar Leblic <cesar.leblic@gmail.com>
2014-08-27 23:24:32 -0400

    Queue and message draft

---
+++ TMP/TMP/src/message.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace TMQ
+{
+	struct MessageBase
+	{
+		virtual ~MessageBase(){}
+	};
+}
+
+template <typename T>
+struct Message : public MessageBase
+{
+	T _data;
+	explicit Message(const T &data)
+		: _data(data)
+	{}
+};
---
+++ TMP/TMP/src/queue.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <memory>
+
+#include "message.hpp"
+
+namespace TMQ
+{
+	class Queue
+	{
+		std::mutex _mutex;
+		std::condition_variable _condition;
+		std::queue<std::shared_ptr<MessageBase>> _queue;
+	public:
+		template<typename T>
+		void push(const T &message)
+		{
+			std::lock_guard<std::mutex> lock(_mutex);
+			_queue.push(std::make_shared<Message<T>>(msg));
+			_condition.notify_one(); 
+		}
+		std::shared_ptr<MessageBase> waitAndPop()
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
+			_condition.wait(lock, [&]{return !_queue.empty(); });
+			auto res = _queue.front();
+			_queue.pop();
+			return res;
+		}
+	};
+}
